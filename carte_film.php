d6c822e30796a97f677de7d99301a0c18ae406f6
TitouanRobe <103327996+TitouanRobe@users.noreply.github.com>
2022-06-08 22:06:14 +0200

    Création de la carte du film

---
+++ carte_film.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Carte Top Gun</title>
+    <link rel="stylesheet" type="text/css" href="./css/style.css"/>
+    <link rel="stylesheet" type="text/css" href="./css/polices.css"/>
+</head>
+<body>
+    <h1>Carte Film</h1>
+    <div class="carte_film">
+        <img src="http://image.tmdb.org/t/p/w300/9IHhJf7n0IxVyLOtn1AY4wgpelh.jpg"alt="Affiche Top Gun">
+        <h3>Top Gun</h3>
+        <div class="carte_film--bloc">
+            <p>1986 - Action</p>
+            <div class="carte_film--bar-container">
+                <div class="carte_film--bar carte_film--bar__rouge"></div>
+                <div class="carte_film--bar carte_film--bar__vert" style="width:80%"></div>
+            </div>
+            
+            <div class="carte_film--likes">
+                <img src="./pictures/green_like.svg"alt="green like">
+                <p>80% aiment</p>
+                <img src="./pictures/red_dislike.svg"alt ="red dislike">
+            </div>
+            
+            </div>
+        </div>
+    </div>
+</body>
+</html>
