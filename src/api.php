cbae2f835d14295a92d9a6bdca4197a41f924515
TitouanRobe <103327996+TitouanRobe@users.noreply.github.com>
2022-06-08 23:28:13 +0200

    Page de recherche

--- src/api.php
+++ src/api.php
@@ -4,7 +4,7 @@
     // définition des constantes
     const API_KEY = "e93f866871a4e28d2076a2475e885408";
     const API_URL = "http://api.themoviedb.org/3/";
-    const IMAGE_URL = "http://image.tmdb.org/t/p/w500";
+    const IMAGE_URL = "http://image.tmdb.org/t/p/w300";
     const HD_IMAGE_URL = "http://image.tmdb.org/t/p/original";
 
     function charger_donnee_api(string $ressource, array $options = NULL): array {
