6a99df52edb062bed9f4817a2525886a04253460
Enzo <enzo.maros@gmail.com>
2022-06-10 15:28:50 +0200

    Ajout de la page d'inscription

--- connexion.php
+++ connexion.php
@@ -10,18 +10,18 @@
     <header class="nav-bar__noir">
         <h1>TOUDOUM</h1>
     </header>
-    <main class="menu-connexion">
-        <h2 class="menu-connexion--titre">Bienvenue sur TOUDOUM</h2>
-        <form class="menu-connexion--formulaire" action="" method="POST">
+    <main class="menu-connexion-inscription">
+        <h2 class="menu-connexion-inscription--titre">Bienvenue sur TOUDOUM</h2>
+        <form class="menu-connexion-inscription--formulaire" action="" method="POST">
             <label for="connexion_utilisateur">Identifiant</label>
-            <input type="text" name="utilisateur" id="connexion_utilisateur" placeholder="Nom">
+            <input type="text" name="utilisateur" id="connexion_utilisateur" placeholder="Nom" required>
             <label for="connexion_mdp">Mot de passe</label>
-            <input type="password" name="mdp" id="connexion_mdp" placeholder="Mot de Passe">
+            <input type="password" name="mdp" id="connexion_mdp" placeholder="Mot de Passe" required>
 
             <button type="submit" name="submit">Se connecter</button>
         </form>
 
-        <p class="menu-connexion--redirection">Nouveau sur le site ? <a href="inscription.php">Inscrivez-vous</a> !</p>
+        <p class="menu-connexion-inscription--redirection">Nouveau sur le site ? <a href="inscription.php">Inscrivez-vous</a> !</p>
     </main>
 </body>
 </html>
