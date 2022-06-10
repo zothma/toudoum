09104d28292a01b3a1bf2d818fa68a26affc536f
Enzo <enzo.maros@gmail.com>
2022-06-10 23:59:37 +0200

    Amélioration affichage cartes films

--- src/carte_film.php
+++ src/carte_film.php
@@ -8,16 +8,20 @@ function generer_carte(string $id, array $film, float $pourcentage)
             <h3><?php echo $film["nom"] ?></h3>
             <div class="carte_film--bloc">
                 <p><?php echo $film["annee_sortie"] . " - " . $film["genre"] ?></p>
-                <div class="carte_film--bar-container">
-                    <div class="carte_film--bar carte_film--bar__rouge"></div>
-                    <div class="carte_film--bar carte_film--bar__vert" style="width:<?php echo $pourcentage ?>%"></div>
+                
+                <div class="carte_film--stats">
+                    <div class="carte_film--bar-container">
+                        <div class="carte_film--bar carte_film--bar__rouge"></div>
+                        <div class="carte_film--bar carte_film--bar__vert" style="width:<?php echo $pourcentage ?>%"></div>
+                    </div>
+    
+                    <div class="carte_film--likes">
+                        <img src="./pictures/green_like.svg" alt="green like">
+                        <p> <?php echo $pourcentage ?>% aiment</p>
+                        <img src="./pictures/red_dislike.svg" alt="red dislike">
+                    </div>
                 </div>
-
-                <div class="carte_film--likes">
-                    <img src="./pictures/green_like.svg" alt="green like">
-                    <p> <?php echo $pourcentage ?>% aiment</p>
-                    <img src="./pictures/red_dislike.svg" alt="red dislike">
-                </div>
+                
             </div>
         </div>
     </a>
