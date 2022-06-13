a6e4e7dcb129761fb855be2f2078ddfc989c50cb
Alexie <alexie.grosbois2002@gmail.com>
2022-06-13 15:24:36 +0200

    ajout functions

--- src/module_base.php
+++ src/module_base.php
@@ -83,7 +83,119 @@
         return $T;
 
     }
-    print_r(recuperer_liste_a_voir(1));
+    
+    function compte_film_aimes_pas($id) {
+        global $link;
+        $sql = mysqli_prepare($link, "SELECT COUNT(id_api) FROM Avis WHERE id_util = ? AND aimer = false;");
+        mysqli_stmt_bind_param($sql, "s", $id);
+
+        if (!(mysqli_stmt_execute($sql))) {
+            echo "ERREUR " . mysqli_error($link);
+            return false; # on retroune false pour dire que ça ne marche pas
+        }
+
+        $row = mysqli_fetch_row(mysqli_stmt_get_result($sql));
+        return (double) $row[0];
+    }
+    
+    function compte_film_aimes($id) {
+        global $link;
+        $sql = mysqli_prepare($link, "SELECT COUNT(id_api) FROM Avis WHERE id_util = ? AND aimer = true;");
+        mysqli_stmt_bind_param($sql, "s", $id);
+
+        if (!(mysqli_stmt_execute($sql))) {
+            echo "ERREUR " . mysqli_error($link);
+            return false; # on retroune false pour dire que ça ne marche pas
+        }
+
+        $row = mysqli_fetch_row(mysqli_stmt_get_result($sql));
+        return (double) $row[0];
+    }
+    
+    function amis_valide($id) {
+        global $link;
+        $sql = mysqli_prepare($link, "SELECT DISTINCT u.nom, u.prenom, u.photo_pp FROM Amis a INNER JOIN Utilisateur u ON a.id_util2 = u.id_util WHERE id_util1 = ? AND valider = True;");
+        mysqli_stmt_bind_param($sql, "i", $id);
+
+        if (!(mysqli_stmt_execute($sql))) {
+            echo "ERREUR " . mysqli_error($link);
+            return false; # on retroune false pour dire que ça ne marche pas
+        }
+
+        $T = [];
+        $resultat = mysqli_stmt_get_result($sql);
+        while( $row = mysqli_fetch_row($resultat) ){
+            array_push($T, [
+                "nom" => $row[0],
+                "prenom" => $row[1],
+                "photo" => $row[2]
+            ]);
+        }
+        return $T;
+    }
+    
+    function amis_pas_valide($id) {
+        global $link;
+        $sql = mysqli_prepare($link, "SELECT DISTINCT u.nom, u.prenom, u.photo_pp FROM Amis a INNER JOIN Utilisateur u ON a.id_util2 = u.id_util WHERE id_util1 = ? AND valider = False;");
+        mysqli_stmt_bind_param($sql, "i", $id);
+
+        if (!(mysqli_stmt_execute($sql))) {
+            echo "ERREUR " . mysqli_error($link);
+            return false; # on retroune false pour dire que ça ne marche pas
+        }
+
+        $T = [];
+        $resultat = mysqli_stmt_get_result($sql);
+        while( $row = mysqli_fetch_row($resultat) ){
+            array_push($T, [
+                "nom" => $row[0],
+                "prenom" => $row[1],
+                "photo" => $row[2]
+            ]);
+        }
+        return $T;
+    }
+    
+    function compte_film_a_voir($id) {
+        global $link;
+        $sql = mysqli_prepare($link, "SELECT COUNT(id_api) FROM Film_à_voir WHERE id_util = ?;");
+        mysqli_stmt_bind_param($sql, "i", $id);
+
+        if (!(mysqli_stmt_execute($sql))) {
+            echo "ERREUR " . mysqli_error($link);
+            return false; # on retroune false pour dire que ça ne marche pas
+        }
+
+        $row = mysqli_fetch_row(mysqli_stmt_get_result($sql));
+        return (double) $row[0];
+    }
+    
+    function avis_laisses($id) {
+        global $link;
+        $sql = mysqli_prepare($link, "SELECT COUNT(commentaire) FROM Avis WHERE id_util = ?;");
+        mysqli_stmt_bind_param($sql, "i", $id);
+
+        if (!(mysqli_stmt_execute($sql))) {
+            echo "ERREUR " . mysqli_error($link);
+            return false; # on retroune false pour dire que ça ne marche pas
+        }
+
+        $row = mysqli_fetch_row(mysqli_stmt_get_result($sql));
+        return (double) $row[0];
+    }
+    
+    function compte_amis_valide($id) {
+        global $link;
+        $sql = mysqli_prepare($link, "SELECT DISTINCT COUNT(*) FROM Amis a INNER JOIN Utilisateur u ON a.id_util2 = u.id_util WHERE id_util1 = '1' AND valider = True;");
+        mysqli_stmt_bind_param($sql, "i", $id);
+
+        if (!(mysqli_stmt_execute($sql))) {
+            echo "ERREUR " . mysqli_error($link);
+            return false; # on retroune false pour dire que ça ne marche pas
+        }
+
+        $row = mysqli_fetch_row(mysqli_stmt_get_result($sql));
+        return (double) $row[0];
+    }
+
 ?>
-
-
