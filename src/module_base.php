02105c5c47b3b86fba7cefb55bab01881c3fc365
Alexie <alexie.grosbois2002@gmail.com>
2022-06-13 09:58:16 +0200

    modif des requêtes

--- src/module_base.php
+++ src/module_base.php
@@ -8,29 +8,32 @@
     
     function avis_aimer($id) {
         global $link;
-        $sql = "SELECT SUM(aimer)/COUNT(*)*100 FROM Avis WHERE id_api = '$id';";
+        $sql = mysqli_prepare($link, "SELECT SUM(aimer)/COUNT(*)*100 FROM Avis WHERE id_api = ?;");
+        mysqli_stmt_bind_param($sql, "s", $id);
 
-        if (!($result = mysqli_query($link, $sql))) {
+        if (!(mysqli_stmt_execute($sql))) {
             echo "ERREUR " . mysqli_error($link);
             return false; # on retroune false pour dire que ça ne marche pas
         }
         
-        $row = mysqli_fetch_row($result);
+        $row = mysqli_fetch_row(mysqli_stmt_get_result($sql));
         return (double) $row[0];
     }
     
+
     function avis_commentaire($id) {
         global $link;
-        $sql = "SELECT commentaire, aimer, prenom, nom, photo_pp FROM Avis NATURAL JOIN Utilisateur WHERE id_api='$id';";
+        $sql = mysqli_prepare($link, "SELECT commentaire, aimer, prenom, nom, photo_pp FROM Avis NATURAL JOIN Utilisateur WHERE id_api= ?;");
+        mysqli_stmt_bind_param($sql, "s", $id);
 
-        if (!($result = mysqli_query($link, $sql))) {
+        if (!(mysqli_stmt_execute($sql))) {
             echo "ERREUR " . mysqli_error($link);
             return false; # on retroune false pour dire que ça ne marche pas
         }
 
         $T = [];
-
-        while( $row = mysqli_fetch_row( $result ) ){
+        $resultat = mysqli_stmt_get_result($sql);
+        while( $row = mysqli_fetch_row($resultat) ){
             array_push($T, [
                 "commentaire" => $row[0],
                 "aimer" => $row[1],
@@ -40,44 +43,47 @@
             ]);
         }
         return $T;
-
     }
-    
+
     function recuperer_liste_vu($id) {
         global $link;
-        $sql = "SELECT id_api FROM Avis WHERE id_util = '$id';";
+        $sql = mysqli_prepare($link, "SELECT id_api FROM Avis WHERE id_util = ?;");
+        mysqli_stmt_bind_param($sql, "s", $id);
 
-        if (!($result = mysqli_query($link, $sql))) {
+        if (!(mysqli_stmt_execute($sql))) {
             echo "ERREUR " . mysqli_error($link);
             return false; # on retroune false pour dire que ça ne marche pas
         }
 
         $T = [];
+        $result = mysqli_stmt_get_result($sql);
 
         while( $row = mysqli_fetch_row( $result )) {
             array_push($T, $row[0]);
         }
         return $T;
     }
+    
 
     function recuperer_liste_a_voir($id) {
         global $link;
-        $sql = "SELECT id_api FROM Film_à_voir WHERE id_util = '$id';";
+        $sql = mysqli_prepare($link, "SELECT id_api FROM Film_à_voir WHERE id_util = ?;");
+        mysqli_stmt_bind_param($sql, "s", $id);
 
-        if (!($result = mysqli_query($link, $sql))) {
+        if (!(mysqli_stmt_execute($sql))) {
             echo "ERREUR " . mysqli_error($link);
             return false; # on retroune false pour dire que ça ne marche pas
         }
 
         $T = [];
-
+        $result = mysqli_stmt_get_result($sql);
         while( $row = mysqli_fetch_row( $result )) {
             array_push($T, $row[0]);
         }
         return $T;
 
     }
-
+    print_r(recuperer_liste_a_voir(1));
 ?>
 
 
