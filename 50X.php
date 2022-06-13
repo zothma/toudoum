5a9153d63420f0612af3358ed27b16f44d4a0556
Enzo <enzo.maros@gmail.com>
2022-06-13 14:17:16 +0200

    Mise en forme du contenu

--- 50X.php
+++ 50X.php
@@ -4,15 +4,15 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300&display=swap" rel="stylesheet">
-    <title>Erreur 500</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/polices.css">
+    <title>Erreur 50X</title>
 </head>
 <body>
     <div class="erreur__center">
         <h2>500 - Une erreur serveur est survenue</h2>
         <p>Merci de réessayer plus tard</p>
-        <img src="er5x.png" alt="">
+        <img src="pictures/er50X.png" alt="Erreur Serveur">
     </div>
 </body>
 </html>
