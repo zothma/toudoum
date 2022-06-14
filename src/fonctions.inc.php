d51210d1470fc6d9f1304e6fb7ebda07e0299d87
Enzo <enzo.maros@gmail.com>
2022-06-14 19:01:39 +0200

    Revert "Ajout de la validation du compte"
    This reverts commit 0558b65a223e0f0781245d55e0c428a60343e267.

--- src/fonctions.inc.php
+++ src/fonctions.inc.php
@@ -4,6 +4,7 @@ require_once 'module_base.php';
 
 function loginUser($email, $pwd)
 {
+    $pwd_crypt = password_hash($pwd, PASSWORD_DEFAULT);
     $correct = user_exists($email, $pwd);
 
     if (!$correct)
@@ -12,16 +13,12 @@ function loginUser($email, $pwd)
     } else {
         $infos_user = recup_info_user($email);
 
-        if ($infos_user["valide"]) {
-            session_start();
-            $_SESSION["userid"] = $infos_user["id"];
-            $_SESSION["userpp"] = $infos_user["photo"];
-            header("location: ../index.php");
-        } else {
-            header("location: ../connexion.php?error=invalid");
-        }
+        session_start();
+        $_SESSION["userid"] = $infos_user["id"];
+        $_SESSION["userpp"] = $infos_user["photo"];
+        header("location: ../index.php");
+        exit();
     }
-    exit();
 }
 
 # Partie Inscription
